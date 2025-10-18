b26ab31b06e7153132961eca7d4600f70515a7c5
Wes Sleeman <weston@wsleeman.com>
2025-10-17 19:00:28 -0700

    Removed HTTPS.

--- DiscordAuth.Bot/Program.cs
+++ DiscordAuth.Bot/Program.cs
@@ -20,11 +20,8 @@ app.MapDefaultEndpoints();
 if (!app.Environment.IsDevelopment())
 {
 	app.UseExceptionHandler("/Error", createScopeForErrors: true);
-	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-	app.UseHsts();
 }
 app.UseStatusCodePagesWithReExecute("/not-found");
-app.UseHttpsRedirection();
 
 app.UseAntiforgery();
 
